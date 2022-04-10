d00807f607824f8e7fd2d5ae5c1e66ebd9e84c9d
apin014 <davindzimar@gmail.com>
2022-04-10 21:02:21 +0700

    update on GameMaster

--- Tubes_OOP_1/app/src/main/java/Tubes_OOP_1/GameMaster.java
+++ Tubes_OOP_1/app/src/main/java/Tubes_OOP_1/GameMaster.java
@@ -2,7 +2,7 @@ package Tubes_OOP_1;
 import java.util.*;
 
 public class GameMaster {
-    public static void game(Scanner console) {
+    public void game(Scanner console) {
         System.out.print("Insert PLAYER 1's name: ");
         String name1 = console.next();
         System.out.print("Insert PLAYER 2's name: ");
@@ -14,32 +14,68 @@ public class GameMaster {
         int player1Switch = -1;
         int player2Switch = -1;
         System.out.printf("Game starting with %s as PLAYER 1 and %s as PLAYER 2\n", player1.getName(), player2.getName());
+        turn(console, player1, player1Move, player1Switch);
+        turn(console, player2, player2Move, player2Switch);
+        if (player1Switch != -1) {
+            player1.switchMonster(player1Switch);
+            player1Switch = -1;
+            if (player2Switch != -1) {
+                player2.switchMonster(player2Switch);
+                player2Switch = -1;
+            } else {
+                player2.getCurMonster().getMoves().get(player2Move).makeAMove(player2.getCurMonster(), player1.getCurMonster());
+            }
+        } else {
+            if (player2Switch != -1) {
+                player2.switchMonster(player2Switch);
+                player2Switch = -1;
+                player1.getCurMonster().getMoves().get(player1Move).makeAMove(player1.getCurMonster(), player2.getCurMonster());
+            } else {
+                Move play1 = player1.getCurMonster().getMoves().get(player1Move);
+                Move play2 = player2.getCurMonster().getMoves().get(player2Move);
+            }
+        }
+        
+    }
+
+    public void turn(Scanner console, Trainer player, int playerMove, int playerSwitch) {
         boolean endTurn = false;
         while (!endTurn) {
-            System.out.printf("| What is %s going to do? |\n", player1.getName());
-                System.out.println("[1] Move\n[2] Switch\n[3] View Monsters Info\n[4] View Game Info");
-                System.out.print(">>");
-                int opt = console.nextInt();
-                if (opt == 1) {
-                    System.out.printf("What is %s's %s going to do?\n", player1.getName(), player1.getCurMonster().getName());
-                    for (int i = 0; i < player1.getCurMonster().getMoves().size(); i++) {
-                        System.out.printf("[%d] %s (%d left)\n", i, player1.getCurMonster().getMoves().get(i).getName(), player1.getCurMonster().getMoves().get(i).getAmmunition());
-                    }
-                    System.out.print(">> ");
-                    player1Move = console.nextInt();
-                    endTurn = true;
-                } else if (opt == 2) {
-                    System.out.printf("Which monster would %s like to switch %s with?\n", player1.getName(), player1.getCurMonster().getName());
-                    for (int i = 0; i < player1.getMonsters().size(); i++) {
-                        if (!player1.getMonsters().get(i).equals(player1.getCurMonster())) {
-                            System.out.printf("[%d] %s", i, player1.getMonsters().get(i));
-                        }
-                    }
-                    System.out.print(">> ");
-                    player1Switch = console.nextInt();
-                    endTurn = true;
+            System.out.printf("\n| What is %s going to do? |\n", player.getName());
+            System.out.println("[1] Move\n[2] Switch\n[3] View Monsters Info\n[4] View Game Info");
+            System.out.print(">>");
+            int opt = console.nextInt();
+            if (opt == 1) {
+                System.out.printf("What is %s's %s going to do?\n", player.getName(), player.getCurMonster().getName());
+                for (int i = 0; i < player.getCurMonster().getMoves().size(); i++) {
+                    System.out.printf("[%d] %s (%d left)\n", i, player.getCurMonster().getMoves().get(i).getName(), player.getCurMonster().getMoves().get(i).getAmmunition());
                 }
+                System.out.print(">> ");
+                playerMove = console.nextInt();
+                endTurn = true;
+            } else if (opt == 2) {
+                System.out.printf("Which monster would %s like to switch %s with?\n", player.getName(), player.getCurMonster().getName());
+                for (int i = 0; i < player.getMonsters().size(); i++) {
+                    if (!player.getMonsters().get(i).equals(player.getCurMonster())) {
+                        System.out.printf("[%d] %s", i, player.getMonsters().get(i));
+                    }
+                }
+                System.out.print(">> ");
+                playerSwitch = console.nextInt();
+                endTurn = true;
+            } else if (opt == 3) {
+                System.out.println("Monsters Info!");
+            } else if (opt == 4) {
+                System.out.println("Game Info!");
+            }
         }
+    }
+
+    public void statusCheck(Monster monster) {
+        StatusCondition.burn(monster);
+        StatusCondition.poison(monster);
+        StatusCondition.sleep(monster);
+        StatusCondition.paralyze(monster);
     }
     /*
     public static void turn(Scanner console, Trainer primary, Trainer secondary) {
